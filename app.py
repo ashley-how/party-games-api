59c8935e8a695b9c7df5175cfa85c754e830f83d
Ashley How <ahow001@e.ntu.edu.sg>
2020-05-16 13:04:16 +0800

    Added db

--- app.py
+++ app.py
@@ -1,13 +1,49 @@
 import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/partyGameDb'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ywwrreehkrldkm:26f8fc64055af96448159aa161df42fbd4d762b0b0545e24e8f42713b21274bf@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/d2eqo77v5v3rri'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class CardDeck(db.Model):
+    __tablename__ = 'cardDeck'
+    id = db.Column(db.Integer, primary_key=True)
+    card = db.Column(db.String(20), unique=True)
+
+    def __init__(self, card):
+        self.card = card
+
+
+@app.route('/addCard', methods=['POST'])
+def addCard():
+    card = request.form['card']
+    if card == '':
+        return {'result': 'Error: An action is required.'}
+    if db.session.query(CardDeck).filter(CardDeck.card == card).count() == 0:
+        data = CardDeck(card)
+        db.session.add(data)
+        db.session.commit()
+        return {'result': 'An action card is added successfully.'}
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -32,6 +68,7 @@ def respond():
     # Return the response in json format
     return jsonify(response)
 
+
 @app.route('/post/', methods=['POST'])
 def post_something():
     param = request.form.get('name')
@@ -41,7 +78,7 @@ def post_something():
         return jsonify({
             "Message": f"Welcome {name} to our awesome platform!!",
             # Add this option to distinct the POST request
-            "METHOD" : "POST"
+            "METHOD": "POST"
         })
     else:
         return jsonify({
@@ -49,9 +86,12 @@ def post_something():
         })
 
 # A welcome message to test our server
+
+
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
