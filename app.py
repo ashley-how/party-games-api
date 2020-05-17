a2eb8f7659d2771d09bed29b5bb728e0f2a66514
Ashley How <ahow001@e.ntu.edu.sg>
2020-05-17 17:39:42 +0800

    Added api to get action cards

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -20,8 +21,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class CardDeck(db.Model):
-    __tablename__ = 'cardDeck'
+class ActionCardDeck(db.Model):
+    __tablename__ = 'ActionCardDeck'
     id = db.Column(db.Integer, primary_key=True)
     card = db.Column(db.String(20), unique=True)
 
@@ -29,68 +30,47 @@ class CardDeck(db.Model):
         self.card = card
 
 
-@app.route('/addCard', methods=['POST'])
-def addCard():
+class CharacterCardDeck(db.Model):
+    __tablename__ = 'CharacterCardDeck'
+    id = db.Column(db.Integer, primary_key=True)
+    card = db.Column(db.String(20), unique=True)
+
+    def __init__(self, card):
+        self.card = card
+
+
+@app.route('/addActionCard', methods=['POST'])
+def addActionCard():
     card = request.form['card']
     if card == '':
         return {'result': 'Error: An action is required.'}
-    if db.session.query(CardDeck).filter(CardDeck.card == card).count() == 0:
-        data = CardDeck(card)
+    if db.session.query(ActionCardDeck).filter(ActionCardDeck.card == card).count() == 0:
+        data = ActionCardDeck(card)
         db.session.add(data)
         db.session.commit()
         return {'result': 'An action card is added successfully.'}
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+
+@app.route('/getAllActionCard')
+def getAllActionCard():
+    cards = ActionCardDeck.query.all()
+    results = [
+        {"result": card.card} for card in cards
+    ]
+    return {'card': results}
 
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
+@app.route('/getActionCard')
+def getActionCard():
+    cardCount = db.session.query(ActionCardDeck).count()
+    randomNum = random.randint(1, cardCount)
+    selectedCard = ActionCardDeck.query.get(randomNum)
+    return {"result": selectedCard.card}
 
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Party Games API server.</h1>"
 
 
 if __name__ == '__main__':
